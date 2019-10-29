ba771505663462fb6d64fa736bd8d6130e58b1d4
pritvor69 <pritvor69@gmail.com>
2019-10-29 20:18:02 +0300

    commit #12

--- backend/shop/models.py
+++ backend/shop/models.py
@@ -3,19 +3,23 @@ from django.db.models.signals import pre_save
 from django.utils.text import slugify
 from transliterate import translit
 
+
 def pre_save_category_slug(sender, instance, *args, **kwargs):
     if not instance.slug:
         slug = slugify(translit(instance.name, reversed=True))
         instance.slug = slug
+
 
 def pre_save_produkt_slug(sender, instance, *args, **kwargs):
     if not instance.slug:
         slug = slugify(translit(instance.title, reversed=True))
         instance.slug = slug
 
+
 def image_folder(instance, filename):
-    filename = instance.slug +'.' + filename.split(".")[1]
+    filename = instance.slug + '.' + filename.split(".")[1]
     return '{0}/{1}'.format(instance.slug, filename)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=120)
@@ -35,6 +39,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
 
 pre_save.connect(pre_save_category_slug, sender=Category)
 pre_save.connect(pre_save_produkt_slug, sender=Product)
